75579dce20e3bf75be61b5e7fb22d40ea94d854c
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-05-02 00:27:15 +0200

    Login creado

--- app/src/main/java/com/example/pachanga_joseasanchezlopez/MainActivity.java
+++ app/src/main/java/com/example/pachanga_joseasanchezlopez/MainActivity.java
@@ -8,8 +8,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
@@ -31,59 +33,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
+        /*etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);*/
+        comprobarAutenticacion();
     }
 
-    /*TODO ESO NO VALE PARA NADA
-        private final ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(
+    private final ActivityResultLauncher<Intent> loginLauncher=registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
             new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
                 @Override
                 public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
-                    onSignInResult(result);
+                    onLoginResult(result);
                 }
             }
     );
 
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+    private void onLoginResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response=result.getIdpResponse();
         if (result.getResultCode()==RESULT_OK){
-            FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+            startActivity(new Intent(this, InicioActivity.class));
+        }else{
+            String msg_error="";
+            if (response == null){
+                msg_error="Es necesario autenticarse";
+            }else if(response.getError().getErrorCode()== ErrorCodes.NO_NETWORK){
+                msg_error="No hay red disponible para autenticarse";
+            }else{
+                msg_error="Error desconocido al autenticarse";
+            }
+            Toast.makeText(this, msg_error, Toast.LENGTH_LONG).show();
+        }
+        finish();
+    }
+
+    public void createSingInIntent(){
+        Intent signIntent=AuthUI.getInstance().createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .build();
+        loginLauncher.launch(signIntent);
+    }
+
+    private void comprobarAutenticacion(){
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(this, InicioActivity.class));
+        }else{
+            createSingInIntent();
         }
     }
-
-    public void createIntentLogin() {
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-        );
-
-        Intent loginIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        loginLauncher.launch(loginIntent);
-    }
-
-    public void emailLink() {
-        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
-                .setAndroidPackageName(
-                .setHandleCodeInApp(true) // This must be set to true
-                .setUrl("https://google.com") // This URL needs to be whitelisted
-                .build();
-
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder()
-                        .enableEmailLinkSignIn()
-                        .setActionCodeSettings(actionCodeSettings)
-                        .build()
-        );
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        loginLauncher.launch(signInIntent);
-    }
-    */
 }
